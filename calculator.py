5cc703c4acbdf69cd613584dca1bf4f1d105fbfe
Jordon Williams <118948296+jordontwill@users.noreply.github.com>
2022-11-28 16:02:11 -0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,48 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+def sub(n1, n2):
+    return n1 - n2
+
+
+def div(n1, n2):
+    return n1 / n2
+
+
+def mul(n1, n2):
+    return n1 * n2
+
+
+operations = {
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": div,
+}
+
+
+def calculator():
+    num1 = float(input("What's the first number? "))
+
+    for symbol in operations:
+        print(symbol)
+
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number? "))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input("Type 'y' to continue: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
